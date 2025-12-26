c577a4b8a7688147cf7c629f81cec95e412aaac9
yourname <test@.tets.com>
2025-12-26 16:30:34 -0500

    test

---
+++ addRemote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+projectName=${1}
+echo "projectName: ${projectName}"
+if [[ ${projectName} == "" ]]; then
+    echo provided empty project name
+    exit 1
+fi
+git remote set-url --add --push origin http://gitlab.dev.local/home-lab/${projectName}
+git remote set-url --add --push origin https://github.com/greenCircuit/${projectName}
+git push --set-upstream origin main
+git remote -v
+
